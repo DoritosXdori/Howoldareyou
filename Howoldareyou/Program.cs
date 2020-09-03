b5b89506fbda17bdb70ea0b3d544bd6499765a9a
Opilane TTHK <opilane@tthk.ee>
2020-09-03 12:20:14 +0300

    first commit

---
+++ Howoldareyou/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Howoldareyou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Good morning?");
+            Console.WriteLine("What`s your name?");
+            string Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello," + Name);
+           Console.WriteLine("What is your year of birth?");        
+            string UserInput;
+            UserInput = Console.ReadLine();
+            int YearOfBirth;
+            //parsing string to interger (number)
+             YearOfBirth = Int32.Parse(UserInput);
+            int Age = 2020 - YearOfBirth;
+            Console.WriteLine("You are "+ Age +"old .");
+
+
+        }
+
+    }
+}
